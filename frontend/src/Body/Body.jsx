4c560e168f5bca187f5c9baaccbeb47b6728fb9b
vladas9 <vladamza63@gmail.com>
2024-07-26 17:10:54 +0300

    feat: Add docker-[Ccompose for whole project
    feat: Add dockerfile for frontend
    
    feat: Add dockerfile for backend
    
    feat: Add ngix to the project

--- frontend/src/Body/Body.jsx
+++ frontend/src/Body/Body.jsx
@@ -1,13 +1,13 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import styles from "./Body.module.css"
 import Parameters from "./Parameters/Parameters";
 import Map from "./Map/map";
 
 
-function Body(){
-    const [sliderValue, setSlidervalue]=useState({
+function Body() {
+    const [sliderValue, setSlidervalue] = useState({
         movement_speed: '1',
-        num_individuals:'100',
+        num_individuals: '100',
         infection_probability: '1',
         infection_radius: '25',
         probability_of_dying: '1',
@@ -15,8 +15,8 @@ function Body(){
     });
     const [responseData, setResponseData] = useState('');
     const [error, setError] = useState(null);
-    const [websocket, setWebsocket]=useState(null);
-    
+    const [websocket, setWebsocket] = useState(null);
+
     const submitHandler = async (e) => {
         console.log(e);
         let requiredDataString = JSON.stringify(sliderValue);
@@ -24,18 +24,18 @@ function Body(){
         if (websocket) {
             websocket.close();
         }
-        startWebSocket(requiredDataString); 
+        startWebSocket(requiredDataString);
     };
     const startWebSocket = (requiredDataString) => {
-        
-        const webSocket = new WebSocket('ws://192.168.1.189:8080/'); // websocket endpoint on server
+
+        const webSocket = new WebSocket('ws://172.18.0.3:8080/'); // websocket endpoint on server
         webSocket.onopen = () => {
             console.log('Connected to server');
             webSocket.send(requiredDataString);
         };
         webSocket.onmessage = (event) => {
             //console.log("Received:", event.data); 
-            let parsed_data=JSON.parse(event.data);
+            let parsed_data = JSON.parse(event.data);
             //  console.log(parsed_data)
             setResponseData(parsed_data);
         };
@@ -47,22 +47,22 @@ function Body(){
         };
         setWebsocket(webSocket);
     };
-    const Reset = () =>{
+    const Reset = () => {
         if (websocket) {
             websocket.close();
         }
         setResponseData('');
     }
-    
-    return(
+
+    return (
         <div className={styles.body}>
-            <Parameters 
+            <Parameters
                 responseData={responseData}
-                sliderValue={sliderValue} 
+                sliderValue={sliderValue}
                 setSlidervalue={setSlidervalue}
-                submitHandler={submitHandler}/>
-            <Map responseData={responseData} resetFunc={Reset}/>
-            
+                submitHandler={submitHandler} />
+            <Map responseData={responseData} resetFunc={Reset} />
+
         </div>
     )
 }
